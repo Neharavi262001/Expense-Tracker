52d1de8162e92b1b924a20d8015488a6e4be57e6
Neharavi262001 <neharavi262001@gmail.com>
2024-10-20 19:07:46 +0530

    intialise project setup and db connection

---
+++ server/index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import { connectDB } from './config/db.js'
+
+dotenv.config()
+connectDB()
+
+const app=express()
+
+const PORT =process.env.PORT || 8080
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port : ${PORT}`)
+})
